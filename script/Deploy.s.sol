f29f7a760194d2fac8f211b70a2c5e7346db9b3b
Lajos Deme <lajosd@protonmail.ch>
2026-03-17 13:20:38 +0000

    implement hook miner script and deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,21 +4,24 @@ pragma solidity 0.8.33;
 import "forge-std/console.sol";
 import "forge-std/Script.sol";
 
-/* 
-- must deploy to unichain
-- first mine the correct hook address
-- verify that the hook address is valid
-- set up everything correctly
-- deploy
- */
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {SuperHook} from "../src/SuperHook.sol";
 
 contract SuperHookDeployer is Script {
 
+    address constant POOL_MANAGER_ADDRESS = 0x00B036B58a818B1BC34d502D3fE730Db729e62AC;
+
     function run() external {
         console.log("starting SuperHook deployment...");
-        address sender = vm.envAddress("SENDER");
 
         uint256 deployerPrivKey = vm.envUint("KEY");
         vm.startBroadcast(deployerPrivKey);
+
+        uint256 salt = 21007;
+        bytes32 saltBytes = bytes32(salt);
+
+        SuperHook superHook = new SuperHook{salt: saltBytes}(IPoolManager(POOL_MANAGER_ADDRESS));
+
+        console.log("super hook deployed to: ", address(superHook));
     }
 }
--- script/MineAddress.s.sol
+++ script/MineAddress.s.sol
@@ -1,2 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.33;
+
+import "forge-std/console.sol";
+import "forge-std/Script.sol";
+
+import {HookMiner} from "../test/HookMiner.sol";
+import {SuperHook} from "../src/SuperHook.sol";
+
+contract SuperHookAddressMiner is Script {
+    address constant POOL_MANAGER_ADDRESS = 0x00B036B58a818B1BC34d502D3fE730Db729e62AC;
+
+    function run() external {
+        bytes memory initCode = abi.encodePacked(
+            type(SuperHook).creationCode,
+            abi.encode(POOL_MANAGER_ADDRESS)
+        );
+
+        // Mine the salt — runs in the script simulation, not onchain.
+        uint256 salt = HookMiner.findSalt(CREATE2_FACTORY, initCode);
+
+        console.log("SALT: ", salt);
+    }
+}
